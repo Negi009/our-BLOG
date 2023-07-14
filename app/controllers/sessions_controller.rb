190590bb521a0a48ba475510808dcd7750997f3b
Negi009 <abhisheknegi009@gmail.com>
2023-07-14 15:28:32 +0530

    create session fucntion and user index page with user image

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in Successfully"
+      redirect_to user
+    else 
+      flash[:alert] = "Invalid Credentails"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logout Successfully"
+    redirect_to root_path
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "User Successfully Created"
-      redirect_to root_path
+      flash[:success] = "User Successfully Created"
+      redirect_to @user
     else
       render 'new'
     end
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       flash[:notice] = "User updated successfully"
-      redirect_to users_path
+      redirect_to @user
     else
       render 'edit'
     end
